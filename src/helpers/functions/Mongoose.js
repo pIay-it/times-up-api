f9c4ff72a0af0bcce42434122d04bc78f0ce45bc
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2021-11-16 18:51:15 +0100

    feat(CI): Finalizing staging deployment

--- src/helpers/functions/Mongoose.js
+++ src/helpers/functions/Mongoose.js
@@ -3,11 +3,10 @@ const Config = require("../../../config");
 
 exports.connect = () => {
     const mongooseOptions = { useUnifiedTopology: true };
-    let URI = `mongodb://`;
     if (Config.db.auth.user && Config.db.auth.pass) {
         mongooseOptions.auth = { authSource: "admin" };
-        URI += `${Config.db.auth.user}:${encodeURIComponent(Config.db.auth.pass)}@`;
+        mongooseOptions.user = Config.db.auth.user;
+        mongooseOptions.pass = encodeURIComponent(Config.db.auth.pass);
     }
-    URI += `localhost/${Config.db.name}`;
-    return mongoose.connect(URI, mongooseOptions);
+    return mongoose.connect(`mongodb://localhost/${Config.db.name}`, mongooseOptions);
 };
